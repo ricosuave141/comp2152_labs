7164b7fd627f08f678ad6dbf809ac1f17e7574d9
AaronSB <aaronshun.balayo@georgebrown.ca>
2026-01-25 08:04:10 -0500

    renewal 2

---
+++ labs1/comp2152_labs/labweek1/lab1.py
@@ -0,0 +1,22 @@
+## aaron balayo
+## 101575606 | T197
+## lab week 1
+
+numbers = [1, 2, 3, 4]
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = a - b ** c // d + a % c
+
+temperature = -20
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+userAge = int(input("Your Age: "))
+userAge += 50
+
+print("Now showing geriatric stuff filtered via Age(s): {}", userAge)
+
